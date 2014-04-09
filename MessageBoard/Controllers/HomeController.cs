72d535f39fa8d01cd37c4e5159ed57087a7479bd
Rob Prouse <rob@prouse.org>
2014-04-09 16:59:41 -0400

    Nearly done second module

--- MessageBoard/Controllers/HomeController.cs
+++ MessageBoard/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MessageBoard.Models;
+using MessageBoard.Services;
 
 namespace MessageBoard.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMailService _mail;
+
+        public HomeController(IMailService mail)
+        {
+            _mail = mail;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
@@ -24,8 +33,17 @@ namespace MessageBoard.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Contact(ContactModel model)
+        {
+            var msg = string.Format("Comment From: {1}{0}Email: {2}{0}Website: {3}{0}Comment: {4}", Environment.NewLine, model.Name, model.Email, model.Website, model.Comment);
+            if ( _mail.SendMail( "rob@prouse.org", "rob@prouse.org", "Web Contact", msg ) )
+            {
+                ViewBag.MailSent = true;
+            }
             return View();
         }
     }
---
+++ MessageBoard/Services/MockMailService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace MessageBoard.Services
+{
+    public class MockMailService : IMailService
+    {
+        #region Implementation of IMailService
+
+        public bool SendMail( string @from, string to, string subject, string body )
+        {
+            Debug.WriteLine( "Sending mail to " + to );
+            return true;
+        }
+
+        #endregion
+    }
+}
