e255e722ad009cb583b29d000f4be84e4a12ec9b
Rob Prouse <rob@prouse.org>
2014-04-10 14:14:17 -0400

    Added an EF Context

--- MessageBoard/Data/MessageBoardContext.cs
+++ MessageBoard/Data/MessageBoardContext.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace MessageBoard.Data
 {
     public class MessageBoardContext : DbContext
     {
-        public MessageBoardContext() : base("")
+        public MessageBoardContext()
+            : base("DefaultConnection")
         {
-            
         }
+
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Reply> Replies { get; set; }
     }
 }
