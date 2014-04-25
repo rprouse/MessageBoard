c110601bacfb9c6509174d43dc929f8686c95108
Rob Prouse <rob@prouse.org>
2014-04-25 13:39:23 -0400

    Finished AngularJS Basics

--- MessageBoard/js/home-index.js
+++ MessageBoard/js/home-index.js
@@ -1,4 +1,20 @@
 ﻿// home-index.js
-function homeIndexController() {
-  alert('Inside homeIndexController');
+function homeIndexController($scope, $http) {
+  $scope.dataCount = 0;
+  $scope.isBusy = true;
+  $scope.data = [];
+
+  $http.get("/api/topics?includeReplies=true")
+    .then(
+      function (result) {
+        // Success
+        angular.copy(result.data, $scope.data);
+      },
+      function () {
+        // Error
+        alert("Could not load topics");
+      })
+     .then(function () {
+       $scope.isBusy = false;
+     });
 }
